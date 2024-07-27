527573fb1f1c3ed88dd805f98a03f7b433f8ce52
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-07-27 10:48:04 +0530

    fix: updated workflows

--- server/resolvers/transaction.res.js
+++ server/resolvers/transaction.res.js
@@ -1,4 +1,4 @@
-import Transaction from "../models/transaction.js"
+import Transaction from '../models/transaction.js'
 const transactionResolver = {
     Query: {
         transactions: async (_, __, context) => {
@@ -6,27 +6,26 @@ const transactionResolver = {
                 if (!context.getUser()) {
                     throw new Error('Unauthorized')
                 }
-                    const userId = await context.getUser().id
+                const userId = await context.getUser().id
                 const transactions = await Transaction.find({ userId })
                 return transactions
-                
             } catch (error) {
                 console.error('Error getting transactions', error)
                 throw new Error('Error getting transactions')
             }
         },
-        transaction: async (_, { transactionId },) => {
+        transaction: async (_, { transactionId }) => {
             try {
                 const transaction = await Transaction.findById(transactionId)
                 return transaction
             } catch (error) {
-                 console.error('Error getting transaction', error)
-                 throw new Error('Error getting transaction')
+                console.error('Error getting transaction', error)
+                throw new Error('Error getting transaction')
             }
         }
     },
     Mutation: {
-        createTransaction: async (_, { input }, context) => { 
+        createTransaction: async (_, { input }, context) => {
             try {
                 const newTransaction = new Transaction({
                     ...input,
@@ -38,14 +37,16 @@ const transactionResolver = {
                 console.error('Error creating transaction', error)
                 throw new Error('Error creating transaction')
             }
-        } ,
-        updateTransaction: async (_, { input }) => { 
+        },
+        updateTransaction: async (_, { input }) => {
             try {
-                const updatedTransaction = await Transaction.findByIdAndUpdate(input.transactionId, input, { new: true })
+                const updatedTransaction = await Transaction.findByIdAndUpdate(input.transactionId, input, {
+                    new: true
+                })
                 return updatedTransaction
             } catch (error) {
-                                console.error('Error updating transaction', error)
-                                throw new Error('Error updating transaction')
+                console.error('Error updating transaction', error)
+                throw new Error('Error updating transaction')
             }
         },
         deleteTransaction: async (_, { transactionId }) => {
@@ -53,10 +54,10 @@ const transactionResolver = {
                 const deleteTransaction = await Transaction.findByIdAndDelete(transactionId)
                 return deleteTransaction
             } catch (error) {
-                console.error("Error deleting transaction", error)
-                throw new Error("Error deleting transaction")
+                console.error('Error deleting transaction', error)
+                throw new Error('Error deleting transaction')
             }
-         }
+        }
     }
 }
 
