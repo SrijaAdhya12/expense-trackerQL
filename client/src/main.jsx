f4002c7f7629f126de334d2d4c2fe1b583f0fbfe
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-07-28 00:03:25 +0530

    feat: deployment changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import { GridBackground } from './components/index.js'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
 const client = new ApolloClient({
-    uri: import.meta.env.VITE_NODE_ENV === "development" ? 'http://localhost:4000/graphql': "/graphql" ,
+    uri: 'http://localhost:4000/graphql',
     cache: new InMemoryCache(),
     credentials: "include"
 })
