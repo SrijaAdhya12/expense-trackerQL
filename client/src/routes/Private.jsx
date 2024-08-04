aaab204e19c37eba1d9f822c3cd6c93a52292e10
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2024-08-04 23:42:36 +0530

    Delete client/src/routes directory

--- client/src/routes/Private.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom'
-// import { GET_AUTHENTICATED_USER } from '../graphql/queries/user.query'
-
-const Private = ({ component }) => {
-    return data.authUser ? component:  <Navigate to="/login" />
-}
-
-export default Private
--- client/src/routes/index.js
+++
@@ -1,2 +0,0 @@
-export { default as AuthRoute } from './Auth'
-export { default as PrivateRoute } from './Private'
