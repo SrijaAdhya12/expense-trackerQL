40bd4dfb57d4b81a333feea1003577b94d33ee04
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-08-16 19:51:36 +0530

    fix: session

--- server/index.js
+++ server/index.js
@@ -38,8 +38,8 @@ app.use(
         cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 7,
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production', 
-            sameSite: 'lax' 
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
         },
         store: store
     }),
