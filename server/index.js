adba62737cca09fe689e0f18b26ee6417d628098
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-08-19 17:30:52 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -36,7 +36,6 @@ app.use(
                 resave: false,
                 saveUninitialized: true,
                 cookie: {
-                        secure: false,
                         maxAge: 1000 * 60 * 60 * 24 * 7
                 },
                 store: store 
