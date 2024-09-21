85b909085b474fcb2d092f21ccf768f041d9b1ac
warmachine028 <75939390+warmachine028@users.noreply.github.com>
2024-09-22 02:06:31 +0530

    docs: env.example

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,12 @@
+client / src / components / Loader.jsx
+import { cn } from '@/lib'
+const Loader = ({ className }) => {
+    return (
+        <div className="flex justify-center items-center">
+            <div
+                className={cn('animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500', className)}
+            />
+        </div>
+    )
+}
+export default Loader
